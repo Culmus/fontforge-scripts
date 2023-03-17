8c50c14f7bd1688e907d9b7b836f82f114348bfd
Maxim Iorsh <iorsh@users.sourceforge.net>
2023-03-18 00:13:32 +0200

    Fix Kern2Comments()

--- fontforge_python/Kern2Comments.py
+++ fontforge_python/Kern2Comments.py
@@ -13,29 +13,29 @@ import utils
 
 def Kern2Comments(unused, font):
 
-    for glyph_class in InitHebrewGlyphData.GetGlyphEquiv():
-        for glyph in glyph_class:
-
-            if glyph not in font:
-                print("Missing glyph: " + glyph)
-                continue
-
-            # For each consonant build a map: vowel_name => X-kerning
-            kernings = {data[2] : data[3] for data in font[glyph].getPosSub("_Diakern") if data[1] == "Pair"}
-
-            for vowel, xkern in kernings.items():
-                vowel_class = next((c for c, l in InitHebrewGlyphData.GetVowelEquiv().items() if vowel in l), None)
-
-                if vowel_class is not None:
-                    utils.SetGlyphCommentProperty(font[glyph], vowel_class, -xkern)
-
-                    # Reset kerning
-                    font[glyph].addPosSub("_Diakern", vowel, 0)
-
-    # "_Diakern" is a dummy kerning table used in UI to facilitate
-    # diacritics positioning. It should be cleaned at the end.
-
     if "_Diakern" in font.gpos_lookups:
+        for glyph_class in InitHebrewGlyphData.GetGlyphEquiv():
+            for glyph in glyph_class:
+
+                if glyph not in font:
+                    print("Missing glyph: " + glyph)
+                    continue
+
+                # For each consonant build a map: vowel_name => X-kerning
+                kernings = {data[2] : data[3] for data in font[glyph].getPosSub("_Diakern") if data[1] == "Pair"}
+
+                for vowel, xkern in kernings.items():
+                    vowel_class = next((c for c, l in InitHebrewGlyphData.GetVowelEquiv().items() if vowel in l), None)
+
+                    if vowel_class is not None:
+                        utils.SetGlyphCommentProperty(font[glyph], vowel_class, -xkern)
+
+                        # Reset kerning
+                        font[glyph].addPosSub("_Diakern", vowel, 0)
+
+        # "_Diakern" is a dummy kerning table used in UI to facilitate
+        # diacritics positioning. It should be cleaned at the end.
+
         font.removeLookup("_Diakern")
 
     font.addLookup("_Diakern", "gpos_pair", ("right_to_left"), (("kern",(("hebr",("dflt")),)),))
