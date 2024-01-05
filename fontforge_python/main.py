031c610d0bffd23e6f1e1dc6875842d655b0437b
Maxim Iorsh <iorsh@users.sourceforge.net>
2024-01-05 23:18:36 +0200

    Separator in Tools menu

--- fontforge_python/main.py
+++ fontforge_python/main.py
@@ -16,7 +16,7 @@ fontforge.registerMenuItem(CreatePrecomposedGlyphs.CreatePrecomposedGlyphs,None,
 fontforge.registerMenuItem(Kern2Comments.Kern2Comments,None,None,"Font",None,"Convert Base-Mark Kernings to Position");
 fontforge.registerMenuItem(GuessMarkToMarkGaps.GuessMarkToMarkGaps,None,None,"Font",None,"Guess Initial Mark To Mark Gaps");
 fontforge.registerMenuItem(Romanization.BuildRomanization,None,None,"Font",None,"Build Romanization Characters");
-fontforge.registerMenuItem(lambda a, b: False, lambda a, b: False,None,"Font",None, "-----------------------");
+fontforge.registerMenuItem("Font",True);
 fontforge.registerMenuItem(AddHebrewGSUB.AddHebrewGSUB,None,None,"Font",None,"Add Hebrew GSUB");
 fontforge.registerMenuItem(AddHebrewGPOS.AddHebrewGPOS,None,None,"Font",None,"Add Hebrew GPOS");
 
